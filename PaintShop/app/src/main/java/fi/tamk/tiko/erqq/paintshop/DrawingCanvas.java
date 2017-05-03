b1fe4c42a5bdf9c1a0354a52bf79062ff9701f33
Eerik <eerik.timonen94@gmail.com>
2017-05-04 01:51:15 +0300

    Add load and save features and javadoc
    Add load and save features and javadoc

--- PaintShop/app/src/main/java/fi/tamk/tiko/erqq/paintshop/DrawingCanvas.java
+++ PaintShop/app/src/main/java/fi/tamk/tiko/erqq/paintshop/DrawingCanvas.java
@@ -1,6 +1,10 @@
 package fi.tamk.tiko.erqq.paintshop;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -8,9 +12,14 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -19,7 +28,15 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DrawingCanvas extends AppCompatActivity {
     DrawingView dv ;
@@ -30,28 +47,21 @@ public class DrawingCanvas extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bundle bundle= getIntent().getExtras();
+
 
         dv = new DrawingView(this);
         dv.setDrawingCacheEnabled(true);
         dv.setColor(Color.RED);
 
-
+        if (bundle !=null){
+            System.out.println(bundle.get("loadedpicture"));
+            if (bundle.containsKey("loadedpicture")){
+                dv.setLoaded(bundle.get("loadedpicture").toString());
+            }
+        }
         setContentView(dv);
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        getWindow().getDecorView().setOnSystemUiVisibilityChangeListener
-                (new View.OnSystemUiVisibilityChangeListener() {
-                    @Override
-                    public void onSystemUiVisibilityChange(int visibility) {
-                        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-                    }
-                });
 
     }
 
@@ -66,32 +76,152 @@ public class DrawingCanvas extends AppCompatActivity {
         inflater.inflate(R.menu.mainmenu,menu);
         return true;
     }
+
+    @Override
+    public void onBackPressed() {
+
+
+        final Dialog dialog= new Dialog(this);
+        dialog.setTitle("Exit");
+        dialog.setContentView(R.layout.exit_dialog);
+        Button exit = (Button) dialog.findViewById(R.id.exit);
+        Button saveExit= (Button) dialog.findViewById(R.id.exitSave);
+        Button cancel = (Button) dialog.findViewById(R.id.cancelButton);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                DrawingCanvas.this.finish();
+            }
+        });
+        saveExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                save(dv.context);
+
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            dialog.dismiss();
+            }
+        });
+        dialog.show();
+
+    }
+    private  File getOutputMediaFile(){
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString()+"/PaintShop");
+
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                return null;
+            }
+        }
+
+        File mediaFile;
+
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator );
+        return mediaFile;
+    }
+    public void save(final Context context){
+        AlertDialog.Builder editalert = new AlertDialog.Builder(DrawingCanvas.this);
+        editalert.setTitle("Please Enter the name with which you want to Save");
+        final EditText input = new EditText(DrawingCanvas.this);
+        editalert.setView(input);
+        editalert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+
+                String name= input.getText().toString();
+                Bitmap bitmap = dv.getDrawingCache();
+
+                File pictureFile = getOutputMediaFile();
+                if (pictureFile == null) {
+                    Log.d("TAG",
+                            "Error creating media file, check storage permissions: ");// e.getMessage());
+                    return;
+                }
+
+
+                File file = new File(pictureFile,name+".png");
+                try
+                {
+                    if(file.exists())
+                    {
+                        file.delete();
+                    }
+                    FileOutputStream ostream = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 0, ostream);
+                     ostream.flush();
+                    ostream.close();
+                    dv.invalidate();
+                    MediaScannerConnection.scanFile(context,new String[] { file.getAbsolutePath() }, null,new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                            Log.wtf("ExternalStorage", "Scanned " + path + ":");
+                            Log.wtf("ExternalStorage", "-> uri=" + uri);
+                        }
+                    });
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }finally
+                {
+
+                    dv.setDrawingCacheEnabled(false);
+                    DrawingCanvas.this.finish();
+                }
+            }
+        });
+
+        editalert.show();
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         dv.mPaint.setXfermode(null);
-        dv.mPaint.setAlpha(0xFF);
+
         super.onOptionsItemSelected(item);
         switch (item.getItemId()){
             case (R.id.red):
+                dv.mPaint.setAlpha(0xFF);
                 dv.mPaint.setColor(Color.RED);
 
                 return true;
             case (R.id.green):
+                dv.mPaint.setAlpha(0xFF);
                 dv.setColor(Color.GREEN);
                 return true;
             case (R.id.blue):
+                dv.mPaint.setAlpha(0xFF);
                 dv.setColor(Color.BLUE);
                 return true;
+            case (R.id.black):
+                dv.mPaint.setAlpha(0xFF);
+                dv.setColor(Color.BLACK);
+                return true;
+            case (R.id.yellow):
+                dv.mPaint.setAlpha(0xFF);
+                dv.setColor(Color.YELLOW);
+                return true;
+            case (R.id.gray):
+                dv.mPaint.setAlpha(0xFF);
+                dv.setColor(Color.GRAY);
+                return true;
             case (R.id.straightLine):
+                dv.mPaint.setAlpha(0xFF);
                 dv.setStraigth(!dv.isStraigth());
                 return true;
             case (R.id.light):
+                dv.mPaint.setAlpha(0xFF);
                 dv.mPaint.setXfermode(new PorterDuffXfermode(
                         PorterDuff.Mode.SRC_ATOP));
                 dv.setColor(Color.parseColor("#FAFAFA"));
                 dv.mPaint.setAlpha(0x90);
                 return true;
             case (R.id.full):
+                dv.mPaint.setAlpha(0xFF);
                 dv.mPaint.setXfermode(new PorterDuffXfermode(
                         PorterDuff.Mode.SRC_ATOP));
                 dv.setColor(Color.parseColor("#FAFAFA"));
--- PaintShop/app/src/main/java/fi/tamk/tiko/erqq/paintshop/MainActivity.java
+++ PaintShop/app/src/main/java/fi/tamk/tiko/erqq/paintshop/MainActivity.java
@@ -1,25 +1,84 @@
 package fi.tamk.tiko.erqq.paintshop;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    final int PERMISSION_REQUEST_ID=0;
+    private static final int SELECT_PICTURE = 1;
+    private String Path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-    }
-    public void newDrawing(View v){
-        Intent intent= new Intent(this,DrawingCanvas.class);
-        startActivity(intent);
+        if ((ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)){
+            String[] listofpermiossions={Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_APN_SETTINGS};
+            ActivityCompat.requestPermissions(this,listofpermiossions,PERMISSION_REQUEST_ID);
+
+        }
+            setContentView(R.layout.activity_main);
 
     }
+    public void newDrawing(View v){
+
+            Intent intent = new Intent(this, DrawingCanvas.class);
+            startActivity(intent);
+
+    }
+
+    public void LoadDrawing(View v){
+        Intent intent = new Intent();
+        intent.setType("image/png");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(Intent.createChooser(intent,
+                "Select Picture"), SELECT_PICTURE);
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                Path = selectedImageUri.toString();
+                System.out.println(Path);
+                Intent intent = new Intent(this, DrawingCanvas.class);
+                intent.putExtra("loadedpicture",Path);
+
+                startActivity(intent);
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            // permission was granted, yay! Do the
+            // contacts-related task you need to do.
+        } else {
+
+
+            Toast.makeText(MainActivity.this, "Permission denied to save and load drawings from your External storage", Toast.LENGTH_LONG).show();
+            MainActivity.this.finish();
+        }
+        return;
+    }
+
 }
